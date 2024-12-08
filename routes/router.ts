e38363a2ac36b9fc78533ff0eed66cb9df3bb540
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-12-07 19:38:54 -0500

    Crear servicios rest: get y post

---
+++ routes/router.ts
@@ -0,0 +1,20 @@
+import { Request, Response, Router } from "express";
+
+const router = Router();
+
+
+router.get('/mensajes', (req: Request, res: Response) => {
+    res.json({
+        ok: true,
+        mensaje: 'Todo esta bien!'
+    })
+});
+
+router.post('/mensajes', (req: Request, res: Response) => {
+    res.json({
+        ok: true,
+        mensaje: 'Todo esta bien!'
+    })
+});
+
+export default router;
