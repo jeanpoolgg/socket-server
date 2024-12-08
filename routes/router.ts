d562ad54843399aa3733446409440c2a1f06d827
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-12-07 19:44:20 -0500

    Obtener informacion del post y argumentos por url

--- routes/router.ts
+++ routes/router.ts
@@ -11,10 +11,28 @@ router.get('/mensajes', (req: Request, res: Response) => {
 });
 
 router.post('/mensajes', (req: Request, res: Response) => {
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    
     res.json({
         ok: true,
-        mensaje: 'Todo esta bien!'
+        cuerpo,
+        de
     })
 });
+
+router.post('/mensajes/:id', (req: Request, res: Response) => {
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    const id = req.params.id;
+    
+    res.json({
+        ok: true,
+        cuerpo,
+        de,
+        id
+    })
+});
+
 
 export default router;
